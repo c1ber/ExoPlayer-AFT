c4e3a4cc4db3ebd7e74bee47aa0e429af102688f
NitroXenon <peter9104@hotmail.com>
2018-03-30 16:04:09 +0800

    [TTV] Allow end of input for all "fully" operations in Mp4Extractor

--- library/core/src/main/java/com/google/android/exoplayer2/extractor/mp4/Mp4Extractor.java
+++ library/core/src/main/java/com/google/android/exoplayer2/extractor/mp4/Mp4Extractor.java
@@ -282,7 +282,7 @@ public final class Mp4Extractor implements Extractor, SeekMap {
     if (atomSize == Atom.DEFINES_LARGE_SIZE) {
       // Read the large size.
       int headerBytesRemaining = Atom.LONG_HEADER_SIZE - Atom.HEADER_SIZE;
-      input.readFully(atomHeader.data, Atom.HEADER_SIZE, headerBytesRemaining);
+      input.readFully(atomHeader.data, Atom.HEADER_SIZE, headerBytesRemaining, true);
       atomHeaderBytesRead += headerBytesRemaining;
       atomSize = atomHeader.readUnsignedLongToLong();
     } else if (atomSize == Atom.EXTENDS_TO_END_SIZE) {
@@ -337,7 +337,7 @@ public final class Mp4Extractor implements Extractor, SeekMap {
     long atomEndPosition = input.getPosition() + atomPayloadSize;
     boolean seekRequired = false;
     if (atomData != null) {
-      input.readFully(atomData.data, atomHeaderBytesRead, (int) atomPayloadSize);
+      input.readFully(atomData.data, atomHeaderBytesRead, (int) atomPayloadSize, true);
       if (atomType == Atom.TYPE_ftyp) {
         isQuickTime = processFtypAtom(atomData);
       } else if (!containerAtoms.isEmpty()) {
@@ -346,7 +346,7 @@ public final class Mp4Extractor implements Extractor, SeekMap {
     } else {
       // We don't need the data. Skip or seek, depending on how large the atom is.
       if (atomPayloadSize < RELOAD_MINIMUM_SEEK_DISTANCE) {
-        input.skipFully((int) atomPayloadSize);
+        input.skipFully((int) atomPayloadSize, true);
       } else {
         positionHolder.position = input.getPosition() + atomPayloadSize;
         seekRequired = true;
@@ -486,7 +486,7 @@ public final class Mp4Extractor implements Extractor, SeekMap {
       skipAmount += Atom.HEADER_SIZE;
       sampleSize -= Atom.HEADER_SIZE;
     }
-    input.skipFully((int) skipAmount);
+    input.skipFully((int) skipAmount, true);
     if (track.track.nalUnitLengthFieldLength != 0) {
       // Zero the top three bytes of the array that we'll use to decode nal unit lengths, in case
       // they're only 1 or 2 bytes long.
@@ -502,7 +502,7 @@ public final class Mp4Extractor implements Extractor, SeekMap {
       while (sampleBytesWritten < sampleSize) {
         if (sampleCurrentNalBytesRemaining == 0) {
           // Read the NAL length so that we know where we find the next one.
-          input.readFully(nalLength.data, nalUnitLengthFieldLengthDiff, nalUnitLengthFieldLength);
+          input.readFully(nalLength.data, nalUnitLengthFieldLengthDiff, nalUnitLengthFieldLength, true);
           nalLength.setPosition(0);
           sampleCurrentNalBytesRemaining = nalLength.readUnsignedIntToInt();
           // Write a start code for the current NAL unit.
